abf1341bc0343affb805314f2debec88f3e94efe
Navya Sree <nsnavyasreei@gmail.com>
2022-04-13 20:22:43 +0530

    modified

--- git-repo-demo/src/com/newbatch/wipro/FirstDay.java
+++ git-repo-demo/src/com/newbatch/wipro/FirstDay.java
@@ -5,6 +5,10 @@ public class FirstDay {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println("My day 1");
+ for(int i=0;i<=10;i++)
+ {
+	 System.out.println(i);
+ }
 	}
 
 }
